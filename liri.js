09544e1fb369ab4959684f116f5a9c5660a12ab8
Jacob Edwards <edwardsjacob1830@gmail.com>
2018-07-14 12:08:28 -0400

    something

--- liri.js
+++ liri.js
@@ -11,9 +11,7 @@ liriCommand = process.argv[2];
 
 function movieSearch(movie) {
 
-  var movieName = process.argv[3];
-
-  movieName = movieName.split("").join("+");
+  var movieName = process.argv.slice(3).join("+");
 
   //var movieName = movieName.split('').join("+");
 
@@ -28,7 +26,8 @@ function movieSearch(movie) {
   request(queryUrl, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var data = JSON.parse(body);
-
+      
+      console.log("\n-------------------\n")
       console.log("\nTitle: " + data.Title +
       "\nRelease Year: " + data.Year +
       "\nIMDB Rating: " + data.imdbRating +
@@ -37,7 +36,9 @@ function movieSearch(movie) {
       "\nLanguage: " + data.Language +
       "\nPlot: " + data.Plot +
       "\nActors: " + data.Actors);
-    };
+    } else {
+      console.log("An error has occured: " + error);
+    }
   });
 };
 
@@ -62,7 +63,7 @@ function displayTweets() {
       }
 
     } else {
-      console.log("Error. Please Try Again");
+      console.log("Error occured: " + err);
     }
   });
 }
@@ -84,6 +85,7 @@ function spotifyThisSong() {
     if (!err) {
       var songInfo = data.tracks.items;
       for (var i = 0; i < 5; i++) {
+        console.log("\n-------------------\n")
         console.log("\nArtist: " + songInfo[i].artists[0].name +
         "\nSong: " + songInfo[i].name +
         "\nAlbum The Song is From: " + songInfo[i].album.name +
@@ -107,14 +109,22 @@ function doWhatItSays() {
 }
 
 switch(liriCommand) {
-  case "my-tweets": displayTweets(); break;
-  case "movie-this": movieSearch(); break;
-  case "spotify-this-song": spotifyThisSong(); break;
-  case "do-what-it-says": doWhatItSays(); break;
+  case "my-tweets": 
+    displayTweets(); 
+    break;
+  case "movie-this": 
+    movieSearch(); 
+    break;
+  case "spotify-this-song": 
+    spotifyThisSong(); 
+    break;
+  case "do-what-it-says": 
+    doWhatItSays(); 
+    break;
   case "default": console.log("\n Select One of the Following" + "\r\n" +
   "1) my-tweets + username: Which will display your latest tweets" + "\r\n" +
   "2) movie-this + movie name: Which will diplay info about a particular movie" + "\r\n" +
   "3) spotify-this-song + sone name: Which will display info about a particular song" + "\r\n" +
-  "4) do-what-it-says: ???");
+  "4) do-what-it-says: ???"); break;
 };
 
